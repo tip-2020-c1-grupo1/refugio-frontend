c470ee22f5560465ab0314a9b3d439296ba168ec
Olmedo Luciano <luciano.federico.olmedo@gmail.com>
2020-04-29 21:00:44 -0300

    Account-Info : Fix error in App.js

--- src/App.js
+++ src/App.js
@@ -11,8 +11,6 @@ import Routes from './Routes';
 
 const browserHistory = createBrowserHistory();
 
-const [googleUser, setGoogleUser] = React.useState('');
-
 validate.validators = {
   ...validate.validators,
   ...validators
@@ -30,14 +28,87 @@ validate.validators = {
 
 export default class App extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      user : {
+        googleId: '',
+        imageUrl: '',
+        // typeOfProfile: '',
+        username: '',
+        firstName: '',
+        lastName: '',
+        email: '',
+        // animals: []
+      }
+    };
+    this.setUser = this.setUser.bind(this);
+    this.fromLocalStorage = this.fromLocalStorage.bind(this);
+    this.setUserInfoFromLocalStorage = this.setUserInfoFromLocalStorage.bind(this);
+    this.checkCredentials = this.checkCredentials.bind(this);
+  }
+
+  componentDidMount() {
+    this.checkCredentials()
+  }
+
+  setUser(user) {
+    this.setState({user});
+  }
+
+  setUserInfoFromLocalStorage() {
+
+    const user = {
+      googleId: this.fromLocalStorage('googleId'),
+      imageUrl: this.fromLocalStorage('imageUrl'),
+      // typeOfProfile: '',
+      username: this.fromLocalStorage('username'),
+      firstName: this.fromLocalStorage('firstName'),
+      lastName: this.fromLocalStorage('lastName'),
+      email: this.fromLocalStorage('email'),
+      // animals: []
+    }
+
+    this.setUser(user);
+  }
+
+  fromLocalStorage(stringProperty) {
+    return window.localStorage.getItem(stringProperty);
+  }
+
+  checkCredentials() {
+    const googleIdLocalStorage = this.fromLocalStorage('googleId');
+    const emailLocalStorage = this.fromLocalStorage('email');
+    const imageUrl = this.state.imageUrl;
+    const googleId = this.state.googleId;
+    const email = this.state.email;
+
+    const basicUserInfoInLocalStorage = emailLocalStorage && googleIdLocalStorage;
+    const basicUserInfoInState = email !== '' && imageUrl !== '' && googleId !== '';
+    
+    if (!basicUserInfoInLocalStorage && !basicUserInfoInState){
+      /* 
+      redirect to sign In , remembering that signIn will call the sso service and
+      use setUser function
+      */
+      console.log('FALTA QUE ME SETEEN LAS CREDENCIALES');
+    } else {
+      if (!basicUserInfoInState) {
+        this.setUserInfoFromLocalStorage();
+      } else {
+        // La tengo en el state y no en localstorage ...
+      }
+      
+    }
+  }
+
   render() {
 
-    setGoogleUser(localStorage.getItem('googleUser'))
 
     return (
       <ThemeProvider theme={theme}>
         <Router history={browserHistory}>
-          <Routes user={googleUser} />
+          <Routes user={this.state.user} />
         </Router>
       </ThemeProvider>
     );
